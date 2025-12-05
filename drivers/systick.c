72ed20327bec7922bba420292ac84b92979bcb7f
odedsab <jsoded@campus.technion.ac.il>
2025-12-05 16:56:53 +0200

    Implemented the SysTick driver. Can initialize SysTick with a user defined frequency Can Get the number of SysTick interrupts since initialization. Can wait for a number of ticks

---
+++ drivers/systick.c
@@ -0,0 +1,105 @@
+#include "systick.h"
+
+#define SYST_CSR        *((volatile uint32_t *)0xE000E010UL) /* SysTick Control and Status Register */
+#define SYST_RVR        *((volatile uint32_t *)0xE000E014UL) /* SysTick Reload value Register */
+#define SYST_CVR        *((volatile uint32_t *)0xE000E018UL) /* SysTick Current value Register */
+#define SYST_CALIB      *((volatile uint32_t *)0xE000E01CUL) /* SysTick Calibration value Register */
+
+/***************** SYS_CSR ******************/
+/* set to 1 to enable SysTick */
+#define SYST_CSR_ENABLE_POS 0U
+#define SYST_CSR_ENABLE_MASK (1U << SYST_CSR_ENABLE_POS)
+
+/* set to 1 -> couting down to 0 asserts SysTick exception request */
+#define SYST_CSR_TICKINT_POS 1U
+#define SYST_CSR_TICKINT_MASK (1U << SYST_CSR_TICKINT_POS)
+
+/* 0 for external source, 1 for processor clock */
+#define SYST_CSR_CLKSOURCE_POS 2U
+#define SYST_CSR_CLKSOURCE_MASK (1U << SYST_CSR_CLKSOURCE_POS)
+
+/* Returns 1 if timer counted to 0 since last read */
+#define SYST_CSR_COUNTFLAG_POS 16U
+#define SYST_CSR_COUNTFLAG_MASK (1U << SYST_CSR_COUTFLAG_POS)
+
+/***************** SYST_RVR ******************/
+/* [23:0] Value to load into the SYST_CVR register when the counter is enabled and when it reaches 0 */
+#define SYST_RVR_RELOAD_POS 0U
+#define SYST_RVR_RELOAD_MASK (0xFFFFFFU << SYST_RVR_RELOAD_POS)
+
+/***************** SYST_CVR ******************/
+/* [23:0] Reads/return the current value of the SysTick counter */
+#define SYST_CVR_RELOAD_POS 0U
+#define SYST_CVR_RELOAD_MASK (0xFFFFFFU << SYST_CVR_RELOAD_POS)
+
+/***************** SYST_CALIB ******************/
+/* [23:0] Reload value for 10ms timing */
+#define SYST_CALIB_TENMS_POS   0U
+#define SYST_CALIB_TENMS_MASK  (0xFFFFFFUL << SYST_CALIB_TENMS_POS)
+
+/* 1 = TENMS value is not exactly 10ms */
+#define SYST_CALIB_SKEW_POS    30U
+#define SYST_CALIB_SKEW_MASK   (1UL << SYST_CALIB_SKEW_POS)
+
+/* 1 = No external reference clock is provided */
+#define SYST_CALIB_NOREF_POS   31U
+#define SYST_CALIB_NOREF_MASK  (1UL << SYST_CALIB_NOREF_POS)
+
+/* Simple global tick counter incremented on each SysTick interrupt */
+static volatile uint32_t systick_tick_counter = 0;
+
+/* SysTick interrupt handler */
+void SysTick_Handler(void)
+{
+    systick_tick_counter++;
+}
+
+/* initialize the SysTick driver with desired tick frequency (Hz) */
+int systick_init(uint32_t ticks_hz) {
+    /* get system clock */
+    uint32_t sysclk_hz = get_system_clock_hz();
+    if(sysclk_hz == 0 || ticks_hz == 0) {
+        return -1; /* error */
+    }
+
+    /* calculate the reload value. 
+    * SysTick counts from reload to 0 (so we need -1)
+    * for example, 1000hz(1ms) ticks with 16MHz sysclk:
+    * reload = (16,000,000 / 1000) - 1 = 15,999
+    */
+    uint32_t reload = (sysclk_hz / ticks_hz) - 1;
+    if (reload > SYST_RVR_RELOAD_MASK) {
+        return -1; /* error: reload value too large */
+    }
+    
+    /* disable SysTick for configuration */
+    SYST_CSR = 0;
+
+    /* set reload value */
+    SYST_RVR = (reload & SYST_RVR_RELOAD_MASK);
+
+    /* reset current value */
+    SYST_CVR = 0;
+
+    /* enable and configure SysTick */    
+    SYST_CSR |= SYST_CSR_ENABLE_MASK    /* enable SysTick */
+            | SYST_CSR_TICKINT_MASK     /* enable SysTick interrupt */
+            | SYST_CSR_CLKSOURCE_MASK;  /* use processor clock */
+    return 0; /* success */
+}
+
+/* Return number of SysTick interrupts since init */
+uint32_t systick_get_ticks(void)
+{
+    return systick_tick_counter;
+}
+
+/* Busy-wait for 'ticks' SysTick interrupts */
+void systick_delay_ticks(uint32_t ticks)
+{
+    uint32_t start = systick_tick_counter;
+    while ((systick_tick_counter - start) < ticks) {
+        /* busy wait */
+    }
+}
+
---
+++ drivers/systick.h
@@ -0,0 +1,38 @@
+#ifndef SYSTICK_H
+#define SYSTICK_H
+
+#include <stdint.h>
+#include "system_clock.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**
+ * @brief Initialize SysTick timer to generate interrupts at specified frequency.
+ * 
+ * @param ticks_hz tick frequency in Hz.
+ * @return int 0 on success, -1 on error (e.g., invalid parameters).
+ */
+int systick_init(uint32_t ticks_hz);
+
+/**
+ * @brief Get the number of SysTick interrupts since initialization.
+ * 
+ * @return uint32_t Number of ticks.
+ */
+uint32_t systick_get_ticks(void);
+
+/**
+ * @brief Busy-wait for specified number of SysTick ticks.
+ * 
+ * @param ticks Number of ticks to wait.
+ */
+void systick_delay_ticks(uint32_t ticks);
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* SYSTICK_H */
